78d56a8bd0aca75701c4c1c1f35314e06c0f0a2c
alwaysandy <andrewdaurio@gmail.com>
2026-02-11 13:38:51 -0500

    Add user model and routes

---
+++ models/UserModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: [true, "Username is already taken"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    }
+})
+
+UserSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10);
+    }
+
+    next();
+});
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+const UserModel = mongoose.model("UserModel", UserSchema);
+module.exports = UserModel;
---
+++ routes/UserRoutes.js
@@ -0,0 +1,40 @@
+const userModel = require("../models/UserModel");
+const express = require('express');
+const UserRoutes = express.Router();
+
+UserRoutes.post('/signup', async (req, res) => {
+    const user = new userModel(req.body);
+    try {
+        const newUser = await user.save();
+        return res.status(201).send({
+            message: "User created successfully",
+            user_id: newUser._id
+        });
+    } catch (err) {
+        return res.status(500).send(
+            err.message
+        );
+    }
+});
+
+UserRoutes.post('/login', async (req, res) => {
+    try {
+        const user = await userModel.findOne({username: req.body.username});
+        const isMatch = await user.comparePassword(req.body.password);
+        if (!isMatch) {
+            return res.status(401).send({
+                status: false,
+                message: "Invalid username or password"
+            });
+        }
+
+        return res.status(201).send("Logged in successfully");
+    } catch (err) {
+        return res.status(500).send({
+            status: false,
+            message: err.message
+        });
+    }
+});
+
+module.exports = UserRoutes;
